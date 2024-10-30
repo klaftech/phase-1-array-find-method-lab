695cac3f4f21ece57e495e5e719a66cd8395c174
Elchonon <ekn400@gmail.com>
2024-10-30 15:56:18 -0400

    lab:

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    const result = record.find((val) => val.result == "W")
+    if(result){
+        return result.year
+    } else {
+        return 
+    }
+}
+
